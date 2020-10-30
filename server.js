a82efe6d04cc5a385ba844b37c24c5d0e9fe5040
David Orona <OronaDavidJ@JohnDeere.com>
2020-10-30 11:56:54 -0500

    Non manual port

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const methodOverride = require('method-override');
@@ -10,6 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/pokemon', require('./controllers/pokemonController.js'));
 app.use('/players', require('./controllers/playersController.js'));
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT, ()=>{
   console.log("Listening");
 });
